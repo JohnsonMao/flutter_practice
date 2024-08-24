5e093eb8a9af04936f25e860efde0181218577b8
JohnsonMao <tutelary.maomao@gmail.com>
2024-08-24 12:45:08 +0800

    feat: add page navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/pages/web_view_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 
@@ -6,10 +7,10 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +42,7 @@ class _MyHomePageState extends State<HomePage> {
               child: ListView.builder(
                 itemCount: 50,
                 itemBuilder: (context, index) {
-                  return _listItemView();
+                  return _listItemView('小貓咪${index + 1}');
                 },
               ),
             ),
@@ -51,49 +52,58 @@ class _MyHomePageState extends State<HomePage> {
     );
   }
 
-  Widget _listItemView() {
-    return Container(
-      margin: EdgeInsets.only(top: 5.h, bottom: 5.h, left: 10.w, right: 10.w),
-      padding: EdgeInsets.only(top: 5.h, bottom: 5.h, left: 10.w, right: 10.w),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black12, width: 0.5.r),
-        borderRadius: BorderRadius.all(Radius.circular(6.r)),
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20.r),
-                child: Image.network(
-                  'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
-                  width: 40.r,
-                  height: 40.r,
-                  fit: BoxFit.fill,
+  Widget _listItemView(String title) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => WebViewPage(title: title)),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 5.h, bottom: 5.h, left: 10.w, right: 10.w),
+        padding:
+            EdgeInsets.only(top: 5.h, bottom: 5.h, left: 10.w, right: 10.w),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black12, width: 0.5.r),
+          borderRadius: BorderRadius.all(Radius.circular(6.r)),
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20.r),
+                  child: Image.network(
+                    'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg',
+                    width: 40.r,
+                    height: 40.r,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 5.w),
-                child: const Text(
-                  '小貓咪',
-                  style: TextStyle(fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 5.w),
+                  child: Text(
+                    title,
+                    style: const TextStyle(fontSize: 20),
+                  ),
                 ),
-              ),
-              const Expanded(child: SizedBox()),
-              Image.asset(
-                'assets/images/star.png',
-                width: 30.r,
-                height: 30.r,
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 5.h),
-            child: const Text(
-              '貓咪是一種迷人又有趣的生物，雖然他們的體型跟外表不像狗狗一樣多變，但多變的花色及逗趣的行為依然讓我們為之著迷，不管是特色鮮明的品種貓，還是最多人飼養的米克斯，只要你喜歡就通通是好貓。',
+                const Expanded(child: SizedBox()),
+                Image.asset(
+                  'assets/images/star.png',
+                  width: 30.r,
+                  height: 30.r,
+                ),
+              ],
             ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.only(top: 5.h),
+              child: const Text(
+                '貓咪是一種迷人又有趣的生物，雖然他們的體型跟外表不像狗狗一樣多變，但多變的花色及逗趣的行為依然讓我們為之著迷，不管是特色鮮明的品種貓，還是最多人飼養的米克斯，只要你喜歡就通通是好貓。',
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/web_view_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WebViewPage extends StatefulWidget {
+  final String title;
+  const WebViewPage({super.key, required this.title});
+
+  @override
+  State<StatefulWidget> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: SafeArea(child: Container()),
+    );
+  }
+}
